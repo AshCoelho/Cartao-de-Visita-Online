ff1580920e311c5e6ed386528aeeff29b908e18c
AshCoelho <167042396+AshCoelho@users.noreply.github.com>
2024-04-20 10:51:53 -0300

    adjust container width

--- script.js
+++ script.js
@@ -2,22 +2,21 @@
 function toggleMode() {
     const html = document.documentElement
 
-    html.classList.toggle('light')
+    html.classList.toggle("light")
+
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+
+
+    //substituir a img
+    if(html.classList.contains("light")) {
+        //se tiver light mode, adcinionar a imagem light
+        img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+        img.setAttribute("src", "./assets/avatar.png")
+        //se tiver sem light mode, manter a imagem normal
+    }
 }
-
-// pegar a tag img
-const img = document.querySelector("#aprofile img")
-
-
-//substituir a img
-if(html.classList.contains('light')) {
-    //se tiver light mode, adcinionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
-} else {
-    img.setAttribute("src", "./assets/avatar.png")
-//se tiver sem kight mode, manter a imagem normal
-}
-
 
 
 
